22367dde6fd1e9294680f3b2aecc64d2f7aa0cbc
Steven Bruman <steven.bruman@yahoo.com>
2018-02-03 13:05:50 -0500

    res is now the resource folder

--- src/Unit.java
+++ src/Unit.java
@@ -19,115 +19,115 @@ public class Unit {
             case AI:
                 switch(name) {
                     case "Marshal":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_marshal.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_marshal.png");
                         break;
 
                     case "General":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_general.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_general.png");
                         break;
 
                     case "Colonel":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_colonel.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_colonel.png");
                         break;
 
                     case "Major":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_major.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_major.png");
                         break;
 
                     case "Captain":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_captain.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_captain.png");
                         break;
 
                     case "Lieutenant":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_lieutenant.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_lieutenant.png");
                         break;
 
                     case "Serjeant":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_serjeant.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_serjeant.png");
                         break;
 
                     case "Miner":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_miner.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_miner.png");
                         break;
 
                     case "Scout":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_scout.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_scout.png");
                         break;
 
                     case "Flag":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_flag.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_flag.png");
                         break;
 
                     case "Bomb":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_bomb.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_bomb.png");
                         break;
 
                     case "Spy":
-                        image = new ImageIcon(getClass().getResource("/../rec/red_spy.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//red_spy.png");
                         break;
 
                     default:
                         image = null;
                         break;
                 }
-                back = new ImageIcon(getClass().getResource("/../rec/red_back.png"));
+                back = new ImageIcon(System.getProperty("user.dir") + "//..//red_back.png");
                 break;
 
             case PLAYER:
                 switch(name) {
                     case "Marshal":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_marshal.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_marshal.png");
                         break;
 
                     case "General":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_general.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_general.png");
                         break;
 
                     case "Colonel":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_colonel.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_colonel.png");
                         break;
 
                     case "Major":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_major.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_major.png");
                         break;
 
                     case "Captain":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_captain.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_captain.png");
                         break;
 
                     case "Lieutenant":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_lieutenant.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_lieutenant.png");
                         break;
 
                     case "Serjeant":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_serjeant.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_serjeant.png");
                         break;
 
                     case "Miner":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_miner.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_miner.png");
                         break;
 
                     case "Scout":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_scout.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_scout.png");
                         break;
 
                     case "Flag":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_flag.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_flag.png");
                         break;
 
                     case "Bomb":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_bomb.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_bomb.png");
                         break;
 
                     case "Spy":
-                        image = new ImageIcon(getClass().getResource("/../rec/blue_spy.png"));
+                        image = new ImageIcon(System.getProperty("user.dir") + "//..//blue_spy.png");
                         break;
 
                     default:
                         image = null;
                         break;
                 }
-                back = new ImageIcon(getClass().getResource("/../rec/blue_back.png"));
+                back = new ImageIcon(System.getProperty("user.dir") + "//..//blue_back.png");
                 break;
 
             default:
