db81cb4ae97fd7357c2945a1a1aa5d974b7a46ce
payyourbill <32520139+payyourbill@users.noreply.github.com>
2018-04-18 12:57:44 -0400

    Add files via upload

---
+++ Randomizer.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class Randomizer
+{
+    public static Random rgen = new Random();
+    public static int getRgen(int i)
+    {
+        return rgen.nextInt(i);
+    }
+}
